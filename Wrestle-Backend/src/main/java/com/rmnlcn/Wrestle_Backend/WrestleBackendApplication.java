cd025c07c409d2d51683de9cb20186c6e384073d
RmNLcN06 <romain.luciano@orange.fr>
2024-12-07 16:56:36 +0100

    Initial commit

---
+++ Wrestle-Backend/src/main/java/com/rmnlcn/Wrestle_Backend/WrestleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Wrestle_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrestleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrestleBackendApplication.class, args);
+	}
+
+}
---
+++ Wrestle-Backend/src/test/java/com/rmnlcn/Wrestle_Backend/WrestleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Wrestle_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrestleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
