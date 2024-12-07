357c5d757ac0d7299b1d9674cfeda8694d658a13
RmNLcN06 <romain.luciano@orange.fr>
2024-12-07 21:51:07 +0100

    Add Swagger annotations to continent controller

--- Wrestle-Backend/src/main/java/com/rmnlcn/Wrestle_Backend/controllers/ContinentController.java
+++ Wrestle-Backend/src/main/java/com/rmnlcn/Wrestle_Backend/controllers/ContinentController.java
@@ -2,6 +2,8 @@ package com.rmnlcn.Wrestle_Backend.controllers;
 
 import com.rmnlcn.Wrestle_Backend.entities.Continent;
 import com.rmnlcn.Wrestle_Backend.services.ContinentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/continents")
+@Tag(name = "Continent API", description = "API to manage continents")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ContinentController {
     @Autowired
@@ -19,26 +22,31 @@ public class ContinentController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all continents", description = "Retrieve all continents from the database.")
     public List<Continent> getAllContinents() {
         return continentService.getAllContinents();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a continent by its ID", description = "Retrieve details of a specific continent.")
     public Continent getContinentById(@PathVariable Long idContinent) {
         return continentService.getContinentById(idContinent);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new continent", description = "Add a new continent to the database.")
     public Continent createContinent(@RequestBody Continent continent) {
         return continentService.createContinent(continent);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing continent", description = "Update detail(s) of a specific continent.")
     public Continent updateContinent(@PathVariable Long idContinent, @RequestBody Continent continentDetails) {
         return continentService.updateContinent(idContinent, continentDetails);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a continent", description = "Remove a continent from the database.")
     public void deleteContinent(@PathVariable Long idContinent) {
         continentService.deleteContinent(idContinent);
     }
